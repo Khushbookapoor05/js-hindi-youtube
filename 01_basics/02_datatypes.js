670d8448c1becd7d29d098e39e2a7a0bf9de00bc
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-02-18 12:23:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all the js code as the newer version 
+
+// alert(3+3) // it gives error because we are using nodejs , not browser
+
+console.log(3+3); console.log("khushi"); //not using the code in a line because the code readability should be high
+console.log(3+3);
+console.log("khushi"); // write like this here the code readability is high
+
+// PRIMITIVE DATA TYPES //
+let name = "khushboo" // string datatype =""
+let age =19 // number datatype =>2 to the power 53
+let inloggedIn = false // boolean datatype = It is the matter of simple yes or no.In this, we get two values here and choose either true/yes or false/no like user logged in or not.
+// bigint is used for big numbers like trading,stockmarkert
+// null= It is a stand alone value in the javascript. It is also a data type inside the javascript but it is the stand alone value in the javascript.
+// undefined = If we are declared the variable but doesn't assign the value.
+// symbol=(unique) When we create many components and and giving them the unique identification to differenciate from other components.
+
+//object
+ 
+console.log(typeof null); // null is an object.
+console.log(typeof undefined); //undefined (it is the type ofitself.)
