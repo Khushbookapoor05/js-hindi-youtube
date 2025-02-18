552aeb07605fb8d877cf83bbec0ef245db8888e6
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-02-18 12:33:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 346587
+let accountEmail = "khushbookapoor@google.com"
+var accountPassword = "568791"
+accountCity = "jaipur"
+let accountState;
+
+// accountId =2
+/* Prefer not to use var 
+   Because of issue in the block scope and fundamental scope 
+ */
+
+
+console.log(accountId);
+accountEmail ="khushikapoor@google.com"
+accountPassword="897654"
+accountCity= "Gujrat"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
