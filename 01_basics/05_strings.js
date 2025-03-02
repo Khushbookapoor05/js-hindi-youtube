ccc4d596c86931ce960bb6b075840ac23ee24663
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-03-02 12:34:31 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,41 +1,73 @@
-const name ="hitesh"
-const repoCount = 50
+ const name ="hitesh"
+ const repoCount = 50
 
-console.log( name + repoCount + " Value"); // this was not a good representation of strings
+ console.log( name + repoCount + " Value"); // this was not a good representation of strings
 
-console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+ console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
 
-/* Declare a string */
+// /* Declare a string */
 
-const gameName = new String ('hiteshhc')
+ const gameName = new String ('hitesh-hc-com')
 
-/*if we want to access a key*/
+// /*if we want to access a key*/
 
-console.log(gameName[0]);
+ console.log(gameName[0]);
 
-/*if we want to access a prototype */
+// /*if we want to access a prototype */
 
-console.log(gameName.__proto__);
+ console.log(gameName.__proto__);
 
-/*if we want to access a length*/
+// /*if we want to access a length*/
  
-console.log(gameName.length);
+ console.log(gameName.length);
 
-/*if we want to access a functions or methods which are inside the prototype*/
+// /*if we want to access a functions or methods which are inside the prototype*/
 
 console.log(gameName.toLowerCase());
 console.log(gameName.toUpperCase());
 
-/*if we want to access a character */
+ /*if we want to access a character */
 
-console.log(gameName.charAt(2));
+console.log(gameName.charAt(2)); 
+
+/*if we want to access a index */
+
 console.log(gameName.indexOf('t'));
 
+/*if we want to access a substring */
+
 const newString = gameName.substring( 0,4)
-console.log(newString);
+console.log(newString); 
+
+/*if we want to use slice to extends a part of string which should  */
+
 
 const anotherString = gameName.slice(-8,4)
-console.log(anotherString );
+console.log(anotherString);
+
+/*if we want to trim the space */
+
+
+const newStringOne = "  hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+/*if we want to replace any item and also asked question */
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20' , '-')) 
+
+console.log(url.includes('hitesh'))
+
+/*if we want to array from a given string */
+
+console.log(gameName.split('-'));
+
+
+
+
+
 
 
 
