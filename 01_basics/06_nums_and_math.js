678b23781a93e5aa3e046eb3f7a59f5aa952857f
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-03-03 15:55:18 +0000

    Numbers and Maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,46 @@
+// const score = 400
+// console.log(score);
+//  const balance = new Number(200)
+//  console.log(balance);
+//  console.log(balance.toString().length);
+ 
+
+//  const anothernNmber = 123.587
+//  console.log(anothernNmber.toPrecision(4));
+
+//  const  otherNumber = 1000000
+//  console.log(otherNumber.toLocaleString('en-IN')); 
+
+
+//  /*****************MATHS********************/
+
+//  console.log(Math);
+// console.log(Math.abs(-4));  // remove the negative sign = 4
+// console.log(Math.round(4.6)); // round off = 5  
+// console.log(Math.ceil(4.2)); // higher or upper value = 5
+// console.log(Math.floor(4.9));// lower or down value = 4
+// console.log(Math.min(4 ,7 ,3 ,5));
+// console.log(Math.max(5 ,8 , 4, 9));
+
+
+console.log(Math.random()); // gives the values in between 0 to 1 *(like 0.9754633)*
+console.log(Math.floor(Math.random()*10) + 1);  // in this by multipling with 10 , We shift the values on the left side   *(like 9.7542368 ; 0.1238546)*
+ // by adding 1 , the values are starting with 1 or greater then 1 and (not with 0 or to avoid 0)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min); // by adding means telling them that start the value from min or it would be greater than min but not less then the min
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
