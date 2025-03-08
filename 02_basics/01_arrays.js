487e9d49be8653a6fbe64688fdef2b40d7169ed6
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-03-08 14:30:54 +0000

    Arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,20 +8,41 @@
 */ 
 
 const myArr = [0, 1 , 2 , 3 , 4 , 5]
-console.log(myArr);
-const myHeroes = ["superman","perman"]
-const myArr2 = new Array(1, 2 ,3 , 4)
-console.log(myArr2[3]);
+// console.log(myArr);
+// const myHeroes = ["superman","perman"]
+// const myArr2 = new Array(1, 2 ,3 , 4)
+// console.log(myArr2[3]);
 
 
 // Array Methods
 
 //myArr.push(6)
-//myArr.pop(6)
-console.log(myArr);
+//myArr.pop()
+//console.log(myArr);
 
-myArr.unshift(9)
-myArr.shift(9)
+//myArr.unshift(9)
+//myArr.shift(9)
 
-console.log(myArr);
+//console.log(myArr);
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf (9));
+//console.log(myArr.indexOf(3));
+ 
+//const newArr = myArr.join() // join will bind the array and change the type of array into string//
+//console.log(myArr);
+//console.log(newArr);
+//console.log(typeof newArr);
+
+// slice , splice
+console.log("A" , myArr ); // slice returns a copy of section an array
+const myn1 = myArr.slice(1,3) // it will takes the starting value but not the ending value that's why result is 1,2
+console.log(myn1);
+console.log("B" , myArr ); // by using slice, the original array remains same   
+const myn2 = myArr.splice(1,3) // it will takes the starting or ending value. Output=1,2,3
+console.log(myn2);
+console.log("C" , myArr); // by using splice, the original array will be menupulated amd removes the splice section 
+
+
+
+
 
