a6d113e838aed95a5785f33a5988b9774a457b82
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-03-09 18:11:58 +0000

    Object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,19 @@
+// Objects:- Objects are declared by two ways :- literals and constructors.
+
+/* Singelton:-
+
+1. If we declare or a create as constructor , then a singelton is an object, it will made the singelton.
+It is the only object in its kind.  
+2. If we declare or a create as literals , then it will not made the singelton.
+*/
+
+// Objects literals
+
+const JsUser = {
+    name : "Hitesh",
+    age : 21,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays :["Monday","Saturday"] 
+}
