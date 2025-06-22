478b530a167790007712ba1fe4a0195ec88705bd
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-06-22 14:31:48 +0000

    05_iterators

--- 05_iterators/three.js
+++ 05_iterators/three.js
@@ -1,3 +1,25 @@
 // High Order Array loops
 // for of
 
+// ["" , "" , ""]
+// [{} , {} , {}]
+
+// const arr = [ 1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     //console.log(num);
+//     }
+
+
+// const grettings = "Hello world!"
+// for (const greet of grettings){
+//     console.log(`Each char is :${greet}`);    
+
+// }
+
+const map = new Map()        // Map object holds key-value pairs and remembers the original insertion order of the keys.
+map.set('IN', "India")      //  Any value (both objects and primitive values) may be used as either a key or a value.
+map.set('USA' , "United States Of America")       // no duplicate values , we get only all unique values
+map.set('Fr' , "France")                       // do not change the order of iterations 
+
+console.log(map);
