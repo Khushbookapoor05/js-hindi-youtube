e5687b840a1875f7f748ae7b1b7a34ddd923d7d8
Khushboo Kapoor <khushbookapoor825@gmail.com>
2025-06-22 13:16:40 +0000

    05_iterators

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -11,19 +11,19 @@ const temperature = 41
 // console.log("Execute");
 // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
+//  const score = 200
 
-// if (score > 100) {
-//     let power = "fly"
-//     console.log(`User power: ${power}`);
-// }
+//  if (score > 100) {
+//      let power = "fly"
+//      console.log(`User power: ${power}`);
+//  }
 
-// console.log(`User power: ${power}`);
+//  console.log(`User power: ${power}`);
 
 
 // const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+// if (balance > 500) console.log("test"),console.log("test2"); // not a good way to write a code
 
 // if (balance < 500) {
 //     console.log("less than 500");
--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -26,3 +26,19 @@ const userEmail = []
 // if (Object.keys(emptyObject).length === 0){
 //     console.log("Object is empty");
 // }
+
+let var1;
+//var1 = 5 ?? 10
+//var1 = null ?? 10 // complex function , so database that function and give the function back
+//var1 = undefined ?? 15
+var1 = null ?? 10 ?? 15   // after null or undefined , we will choose the first value for the output
+console.log(var1);
+
+//  Terniary operators
+
+// conditions ? true : false
+
+const iceTeaPrice = 100
+//iceTeaPrice >= 80 ? console.log(" less than 80 ") : console.log(" more than 80 ")  // hence we know that the iceteaprice is 100 which is >= 80 ,so that condition is true , we print the true condition.  
+
+iceTeaPrice <= 80 ? console.log(" less than 80 ") : console.log(" more than 80 ")   // condition is false
